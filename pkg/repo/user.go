270d7425392ebddf7cab2f87aa690c8e4acbfb13
Ananda Wiradharma <anandawira754@gmail.com>
2022-02-18 21:28:22 +0700

    create Insert method in user repo

--- pkg/repo/user.go
+++ pkg/repo/user.go
@@ -1,9 +1,25 @@
 package repo
 
-import "gorm.io/gorm"
+import (
+	"context"
 
-type bookRepository struct {
-	Conn *gorm.DB
+	"github.com/anandawira/anandapay/pkg/model"
+	"gorm.io/gorm"
+)
+
+type userRepository struct {
+	DB *gorm.DB
 }
 
-func NewBookRepository(Conn *gorm.DB)
+func NewUserRepository(db *gorm.DB) model.UserRepository {
+	return &userRepository{DB: db}
+}
+
+func (m *userRepository) Insert(ctx context.Context, fullname, email, hashedPassword string) error {
+	user := model.User{FullName: fullname, Email: email, HashedPassword: hashedPassword, IsVerified: false}
+	result := m.DB.Create(&user)
+	if result.Error != nil {
+		return result.Error
+	}
+	return nil
+}
