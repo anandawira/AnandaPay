a3412afe25982bdd0460979703bf0eec0f678dfa
Ananda Wiradharma <anandawira754@gmail.com>
2022-02-18 20:05:46 +0700

    connect to database on main.go

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/anandawira/anandapay/config"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	r := gin.Default()
+	config.Connect()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
