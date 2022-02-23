59587138e7409462e33eab1ecdccf13e045619f2
Ananda Wiradharma <anandawira754@gmail.com>
2022-02-23 13:13:47 +0700

    add email to binding validation

--- pkg/handler/userJSON.go
+++ pkg/handler/userJSON.go
@@ -2,6 +2,6 @@ package handler
 
 type RegisterRequest struct {
 	Fullname string `form:"fullname" binding:"required"`
-	Email    string `form:"email" binding:"required"`
+	Email    string `form:"email" binding:"required,email"`
 	Password string `form:"password" binding:"required"`
 }
