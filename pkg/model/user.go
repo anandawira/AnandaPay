4d430a7714209a2f7fc98bb78c1e6f26a42c35b7
Ananda Wiradharma <anandawira754@gmail.com>
2022-02-23 21:52:41 +0700

    fix wrong gorm tag

--- pkg/model/user.go
+++ pkg/model/user.go
@@ -9,7 +9,7 @@ import (
 type User struct {
 	gorm.Model
 	FullName       string `json:"fullname" gorm:"not null"`
-	Email          string `json:"email" gorm:"unique,not null"`
+	Email          string `json:"email" gorm:"unique;not null"`
 	HashedPassword string `json:"password" gorm:"not null"`
 	IsVerified     bool   `json:"isVerified" gorm:"not null"`
 }
