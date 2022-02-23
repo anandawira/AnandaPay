c2bdbf10147988ebc9a51b2bcbdbe65b833f4fb9
Ananda Wiradharma <anandawira754@gmail.com>
2022-02-23 16:46:00 +0700

    add logging on database transactions

--- config/database.go
+++ config/database.go
@@ -6,6 +6,7 @@ import (
 	"github.com/anandawira/anandapay/pkg/model"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 var DB *gorm.DB
@@ -13,7 +14,8 @@ var DB *gorm.DB
 func Connect() *gorm.DB {
 	// Hardcore, later change to env variable
 	dsn := "root:example@tcp(127.0.0.1:3306)/anandapay?charset=utf8mb4&parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{Logger: logger.Default.LogMode(logger.Info)})
+	
 	if err != nil {
 		log.Fatal(err.Error())
 	}
