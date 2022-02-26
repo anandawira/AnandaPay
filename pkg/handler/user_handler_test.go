539448803ff79c9d26fe6d98e3c8e33b7462232d
Ananda Wiradharma <anandawira754@gmail.com>
2022-02-26 14:43:56 +0700

    add helper function

--- pkg/handler/user_handler_test.go
+++ pkg/handler/user_handler_test.go
@@ -26,9 +26,46 @@ func (ts *UserHandlerTestSuite) SetupSuite() {
 	ts.handler = &UserHandler{
 		userUsecase: ts.mockUsecase,
 	}
+	gin.SetMode(gin.TestMode)
+}
+
+func createPostContext(body map[string]string) (*gin.Context, *httptest.ResponseRecorder) {
+	form := url.Values{}
+	for key, val := range body {
+		form.Set(key, val)
+	}
+
+	req := httptest.NewRequest("POST", "/users", strings.NewReader(form.Encode()))
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	rec := httptest.NewRecorder()
+	c, _ := gin.CreateTestContext(rec)
+	c.Request = req
+	return c, rec
+}
+
+func assertResponse(t testing.TB, code int, message string, recorder *httptest.ResponseRecorder) {
+	t.Helper()
+
+	want := gin.H{
+		"message": message,
+	}
+
+	var got gin.H
+	err := json.Unmarshal(recorder.Body.Bytes(), &got)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+
+	assert.Equal(t, code, recorder.Code)
+	assert.Equal(t, want, got)
 }
 
 func (ts *UserHandlerTestSuite) TestRegister() {
+	var bodySample map[string]string = map[string]string{
+		"fullname": "testname",
+		"email":    "test@gmail.com",
+		"password": "testpassword",
+	}
 	ts.T().Run("It should return with StatusOK", func(t *testing.T) {
 		ts.mockUsecase.On(
 			"Register",
@@ -38,44 +75,37 @@ func (ts *UserHandlerTestSuite) TestRegister() {
 			mock.AnythingOfType("string"),
 		).Return(nil).Once()
 
-		form := url.Values{}
-		form.Set("fullname", "test name")
-		form.Set("email", "test@gmail.com")
-		form.Set("password", "testpassword")
-
-		req := httptest.NewRequest("POST", "/users", strings.NewReader(form.Encode()))
-		req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-		rec := httptest.NewRecorder()
-		c, _ := gin.CreateTestContext(rec)
-		c.Request = req
+		c, rec := createPostContext(bodySample)
 
 		ts.handler.RegisterPost(c)
-		assert.Equal(t, http.StatusOK, rec.Result().StatusCode)
+
+		assertResponse(t, http.StatusOK, "User registered to the database successfully.", rec)
 	})
 
 	ts.T().Run("It should return with StatusBadRequest on invalid input", func(t *testing.T) {
-		req := httptest.NewRequest("POST", "/users", nil)
-		rec := httptest.NewRecorder()
-		c, _ := gin.CreateTestContext(rec)
-		c.Request = req
+		c, rec := createPostContext(map[string]string{})
 
 		ts.handler.RegisterPost(c)
-		assert.Equal(t, http.StatusBadRequest, rec.Result().StatusCode)
+
+		assertResponse(t, http.StatusBadRequest, "Parameter validation failed.", rec)
 	})
 
 	ts.T().Run("It should return with StatusBadRequest on duplicate email", func(t *testing.T) {
-		form := url.Values{}
-		form.Set("fullname", "test name")
-		form.Set("email", "duplicate@gmail.com")
-		form.Set("password", "testpassword")
-
 		ts.mockUsecase.On(
 			"Register",
 			mock.Anything,
 			mock.AnythingOfType("string"),
 			mock.AnythingOfType("string"),
 			mock.AnythingOfType("string"),
-		).Return(errors.New("Duplicate")).Once()
+		).Return(errors.New("Email already in use.")).Once()
+
+		c, rec := createPostContext(bodySample)
+
+		ts.handler.RegisterPost(c)
+
+		assertResponse(t, http.StatusBadRequest, "Email already in use.", rec)
+	})
+}
 
 		req := httptest.NewRequest("POST", "/users", strings.NewReader(form.Encode()))
 		req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
