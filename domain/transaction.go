77b5bab0078ebe787233148061df77d6b23d8c1f
Ananda Wiradharma <anandawira754@gmail.com>
2022-02-28 00:10:36 +0700

    add wallet model

--- domain/transaction.go
+++ domain/transaction.go
@@ -1,6 +1,8 @@
 package domain
 
 type Wallet struct {
-	ID string 
-	UserID string
+	ID      string `gorm:"unique;not null"`
+	UserID  uint
+	User    User
+	Balance int64 `gorm:"not null;default:0"`
 }
